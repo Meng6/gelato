04cd94d0ba19861ddf1ecfba92a04cc6d60c57d3
Meng Li <34143965+Meng6@users.noreply.github.com>
2022-12-15 11:16:23 -0600

    Update SQL queries and print pairs

--- src/query/mgdb/sql_main.py
+++ src/query/mgdb/sql_main.py
@@ -1,13 +1,19 @@
-def save_ancestors(ancestors, pid, output_file):
+def save_ancestors(ancestors, pid, output_file, pairs_flag):
     fout = open(output_file, mode="w", encoding="utf-8")
-    fout.write("The ancestors of " + str(pid) + " are: (" + str(len(ancestors)) + " ancestors)\n")
+    if pairs_flag:
+        fout.write("The ancestor pairs (student, advisor) of " + str(pid) + " are: (" + str(len(ancestors)) + " ancestor pairs)\n")
+    else:
+        fout.write("The ancestors of " + str(pid) + " are: (" + str(len(ancestors)) + " ancestors)\n")
     fout.write("\n".join(map(str, ancestors)))
     fout.close()
     return
 
-def save_descendants(descendants, pid, output_file):
+def save_descendants(descendants, pid, output_file, pairs_flag):
     fout = open(output_file, mode="w", encoding="utf-8")
-    fout.write("The descendants of " + str(pid) + " are: (" + str(len(descendants)) + " descendants)\n")
+    if pairs_flag:
+        fout.write("The descendant pairs (student, advisor) of " + str(pid) + " are: (" + str(len(descendants)) + " descendant pairs)\n")
+    else:
+        fout.write("The descendants of " + str(pid) + " are: (" + str(len(descendants)) + " descendants)\n")
     fout.write("\n".join(map(str, descendants)))
     fout.close()
     return
@@ -20,18 +26,18 @@ def unary_search_ancestors(pid, conn, output_file):
     # Recursive CTE
     sql_query = """
     WITH RECURSIVE ancestors AS (
-	    select advisor from advised a, dissertation d where a.did=d.did and author = """ + str(pid) + """
+	    SELECT advisor FROM advised a, dissertation d WHERE a.did=d.did AND author = """ + str(pid) + """
 	    UNION
-	    select a.advisor from ancestors an, advised a, dissertation d where a.did=d.did and d.author=an.advisor
+	    SELECT a.advisor FROM ancestors an, advised a, dissertation d WHERE a.did=d.did AND d.author=an.advisor
 	    )
-    SELECT advisor, name from ancestors left join person p on p.pid = advisor
+    SELECT advisor, name FROM ancestors LEFT JOIN person p ON p.pid = advisor
     """
 
     cursor.execute(sql_query)
     ancestors = cursor.fetchall()
     cursor.close()
     
-    save_ancestors(ancestors, pid, output_file)
+    save_ancestors(ancestors, pid, output_file, False)
     return
 
 def binary_search_ancestors(pid, conn, output_file):
@@ -48,15 +54,15 @@ def binary_search_ancestors(pid, conn, output_file):
     SELECT author, p1.name, advisor, p2.name
     FROM
     	ancestors 
-    	left join person p1 on p1.pid = author
-    	left join person p2 on p2.pid = advisor
+    	LEFT JOIN person p1 ON p1.pid = author
+    	LEFT JOIN person p2 ON p2.pid = advisor
     """
 
     cursor.execute(sql_query)
     ancestors = cursor.fetchall()
     cursor.close()
     
-    save_ancestors(ancestors, pid, output_file)
+    save_ancestors(ancestors, pid, output_file, True)
     return
 
 def unary_search_descendants(pid, conn, output_file):
@@ -65,18 +71,18 @@ def unary_search_descendants(pid, conn, output_file):
     # Recursive CTE
     sql_query = """
     WITH RECURSIVE descendants AS (
-        select author from dissertation d, advised a where a.did=d.did and advisor = """ + str(pid) + """
+        SELECT author FROM dissertation d, advised a WHERE a.did=d.did AND advisor = """ + str(pid) + """
         UNION
-        select di.author from dissertation di, advised a, descendants d where a.did=di.did and a.advisor=d.author
+        SELECT d.author FROM dissertation d, advised a, descendants de WHERE a.did=d.did AND a.advisor=de.author
         )
-    SELECT author, name from descendants left join person p on p.pid = author
+    SELECT author, name FROM descendants LEFT JOIN person p ON p.pid = author
     """
 
     cursor.execute(sql_query)
     descendants = cursor.fetchall()
     cursor.close()
 
-    save_descendants(descendants, pid, output_file)
+    save_descendants(descendants, pid, output_file, False)
     return
 
 def binary_search_descendants(pid, conn, output_file):
@@ -85,20 +91,20 @@ def binary_search_descendants(pid, conn, output_file):
     # Recursive CTE
     sql_query = """
     WITH RECURSIVE descendants AS (
-        select author, advisor from dissertation d, advised a where a.did=d.did and advisor = """ + str(pid) + """
+        SELECT author, advisor FROM dissertation d, advised a WHERE a.did=d.did AND advisor = """ + str(pid) + """
         UNION
-        select d.author, a.advisor from dissertation d, advised a, descendants de where a.did=d.did and a.advisor=de.author
+        SELECT d.author, a.advisor FROM dissertation d, advised a, descendants de WHERE a.did=d.did AND a.advisor=de.author
         )
     SELECT author, p1.name, advisor, p2.name
     FROM
     	descendants 
-    	left join person p1 on p1.pid = author
-    	left join person p2 on p2.pid = advisor
+    	LEFT JOIN person p1 on p1.pid = author
+    	LEFT JOIN person p2 on p2.pid = advisor
     """
 
     cursor.execute(sql_query)
     descendants = cursor.fetchall()
     cursor.close()
 
-    save_descendants(descendants, pid, output_file)
+    save_descendants(descendants, pid, output_file, True)
     return
