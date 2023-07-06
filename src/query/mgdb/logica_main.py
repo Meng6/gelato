e5cbc201c7f020c2f3c0302d71f1f05a9c1a60c5
Meng Li <34143965+Meng6@users.noreply.github.com>
2023-07-06 15:58:39 -0500

    Add logica pkg; update unary_search_ancestors with logica

--- src/query/mgdb/logica_main.py
+++ src/query/mgdb/logica_main.py
@@ -2,21 +2,22 @@
 def unary_search_ancestors(params, database):
 
     logica_query = """
-
     @Engine("sqlite");
     @AttachDatabase("mgdb", "{database}");
     @Dataset("advised");
     @Dataset("person");
     @Dataset("dissertation");
 
-    #Anc(Advisor,Student)
     Adv_Stu(advisor:, student:author) :- Advised(did:x, advisor:),Dissertation(did:y, author:), x=y;
-    Anc(advisor:, student:) :- Adv_Stu(advisor:, student:);
-    Anc(advisor:x, author:m) :- Anc(advisor:x, student:y), Adv_Stu(advisor:z, student:m), y=z, m={pid};
-    
+
+    @Recursive(Anc,33);
+    Anc(ancestor:advisor, student:m) distinct :- Adv_Stu(advisor:, student:m), m={pid};
+    Anc(ancestor:x, student:l) distinct:- Adv_Stu(advisor:x, student:y),Anc(ancestor:z, student:l), y=z;
+
+    Anc_with_Name(ancestor_id:x,ancestor_name:n) :-Anc(ancestor:x, student:l), person(pid:y,name:n),x=y;
     """.format(database=database, pid=params["pid"])
 
-    output_name = "Anc"
+    output_name = "Anc_with_Name"
     columns = ["advisor", "author"]
 
     return logica_query, output_name, columns
